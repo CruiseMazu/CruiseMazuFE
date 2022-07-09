8956b65111b847b7e9bab99e8a991d3638b72d5e
Warren Tait <warren@moanallc.com>
2022-07-08 18:44:45 -0700

    Build a working application.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1>Welcome to CruiseMazu!</h1>
+      <h1> Welcome to CruiseMazu! </h1>
+      <p> You are successfully running it on your machine.</p>
+      <div>Counter: {counter} </div>
+      <button
+        onClick={() =>
+          setCounter((prevCount) => {
+            return prevCount + 1;
+          })
+        }
+      >
+        Increment!
+      </button>
+      <button
+        onClick={() =>
+          setCounter((prevCount) => {
+            return prevCount - 1;
+          })
+        }
+      >
+        Decrement!
+      </button>
     </div>
   );
 };
